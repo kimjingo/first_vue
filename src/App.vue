1345b552a12cdb44b534e776188ff78480b165fe
Jingoo Kim <kimjingo@gmail.com>
2021-04-01 16:45:40 -0700

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="(menu, index) in menus" :key="index" href="">{{ menu }}</a>
+  </div>
+  <div v-for="(item, index) in products" :key="index + item">
+    <h4>{{ index }} : {{ item }}</h4>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      menus: ["HOME", "SHOP", "ABOUT"],
+      products: ["a", "b", "c"],
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
@@ -21,6 +27,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
